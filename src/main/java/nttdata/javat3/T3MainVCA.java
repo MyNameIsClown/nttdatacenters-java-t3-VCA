6782978a02690a10a563333377b9dad361cb8c96
MyNameIsClown <96545817+MyNameIsClown@users.noreply.github.com>
2022-08-15 18:47:41 +0200

    Creacion de clases y paquetes

---
+++ src/main/java/nttdata/javat3/T3MainVCA.java
@@ -0,0 +1,10 @@
+package nttdata.javat3;
+
+public class T3MainVCA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/nttdata/javat3/business/Person.java
@@ -0,0 +1,5 @@
+package nttdata.javat3.business;
+
+public class Person {
+
+}
